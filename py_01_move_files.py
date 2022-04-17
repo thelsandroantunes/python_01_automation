764d124f07c0d6e7f50c2ed08f9447f34761f42c
Thelsandro Antunes <thelsandro.developer@gmail.com>
2022-04-17 11:49:46 -0400

    update

--- py_01_move_files.py
+++ py_01_move_files.py
@@ -1,21 +1,50 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import json
+
 import os
 import time
 import shutil
+
+# ========== Function Select extension files Start========== #
 
 
 class FileHandler(FileSystemEventHandler):
     def on_modified(self, event):
         for file in os.listdir(folder_to_track):
-            file_folder = os.path.join(folder_to_track, file)
-            shutil.move(file_folder, destination_folder)
+            if file.endswith('.pdf') or file.endswith('.doc') or file.endswith('.txt') or file.endswith('.ppt'):
+                os.rename(folder_to_track+'//'+file, pdf_directory+'//'+file)
+            elif file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg') or file.endswith('.PNG') or file.endswith('.svg') or file.endswith('.eps'):
+                os.rename(folder_to_track+'//'+file, img_directory+'//'+file)
+            if file.endswith('.mp4') or file.endswith('.avi') or file.endswith('.vob') or file.endswith('.flv') or file.endswith('.mpeg') or file.endswith('.m4v'):
+                os.rename(folder_to_track+'//'+file, mp4_directory+'//'+file)
+            if file.endswith('.epub') or file.endswith('.mobi'):
+                os.rename(folder_to_track+'//'+file, book_directory+'//'+file)
+            elif file.endswith('.exe'):
+                shutil.move(folder_to_track+'//'+file, exe_directory+'//'+file)
+            else:
+                print('Other file format: ' + file)
+
+            #file_folder = os.path.join(folder_to_track, file)
+            #shutil.move(file_folder, destination_folder)
+# ========== Function Select extension files End========== #
 
 
-folder_to_track = "C://Users//thels//Downloads//teste"
-destination_folder = "C://Users//thels//OneDrive//Documentos"
+# ========== Folder Original Start ========== #
+folder_to_track = " pathway "
+# ========== Folder Original End ========== #
 
+# ========== Moving the files Start ========== #
+pdf_directory = "pathway"
+img_directory = "pathway"
+mp4_directory = "pathway"
+book_directory = "pathway"
+other_directory = "pathway"
+
+exe_directory = "pathway"
+code_directory = "pathway"
+# ========== Moving the files End ========== #
+
+# ========== Event Start ========== #
 event_handler = FileHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
@@ -27,3 +56,5 @@ try:
 except KeyboardInterrupt:
     observer.stop()
 observer.join()
+
+# ========== Event End ========== #
